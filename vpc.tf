d5f6dbba0992818723dde3bee0e389ee2e95aa5c
GEROME84 <uszone3@gmail.com>
2018-03-11 19:58:34 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -76,11 +76,11 @@ resource "aws_security_group" "nat" {
 
 resource "aws_instance" "nat" {
     ami = "ami-30913f47" # this is a special ami preconfigured to do NAT
-    availability_zone = "eu-west-1a"
+    availability_zone = "us-west-1a"
     instance_type = "t2.midium"
     key_name = "${var.aws_key_name}"
     vpc_security_group_ids = ["${aws_security_group.nat.id}"]
-    subnet_id = "${aws_subnet.eu-west-1a-public.id}"
+    subnet_id = "${aws_subnet.us-west-1a-public.id}"
     associate_public_ip_address = true
     source_dest_check = false
 
@@ -101,7 +101,7 @@ resource "aws_subnet" "us-west-1a-public" {
     vpc_id = "${aws_vpc.default.id}"
 
     cidr_block = "${var.public_subnet_cidr}"
-    availability_zone = "eu-west-1a"
+    availability_zone = "us-west-1a"
 
     tags {
         Name = "Public Subnet"
@@ -122,8 +122,8 @@ resource "aws_route_table" "us-west-1a-public" {
 }
 
 resource "aws_route_table_association" "us-west-1a-public" {
-    subnet_id = "${aws_subnet.eu-west-1a-public.id}"
-    route_table_id = "${aws_route_table.eu-west-1a-public.id}"
+    subnet_id = "${aws_subnet.us-west-1a-public.id}"
+    route_table_id = "${aws_route_table.us-west-1a-public.id}"
 }
 
 /*
@@ -140,7 +140,7 @@ resource "aws_subnet" "us-west-1a-private" {
     }
 }
 
-resource "aws_route_table" "eu-west-1a-private" {
+resource "aws_route_table" "us-west-1a-private" {
     vpc_id = "${aws_vpc.default.id}"
 
     route {
