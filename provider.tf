8fb9db7892f99b7f5d8a9fb4bb069633d5274f15
GEROME84 <uszone3@gmail.com>
2018-03-11 19:21:52 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
-provider "aws" { 
-    region = "${var.AWS_REGION}"
+# Configure the AWS Provider
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
 }
